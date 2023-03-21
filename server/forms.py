faa6063395155dcbb7069598ce28c45946663f5f
mariojlunam <71411490+mariojlunam@users.noreply.github.com>
2023-03-21 14:05:14 +0000

    Update forms.py

--- server/forms.py
+++ server/forms.py
@@ -2,7 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import FloatField, IntegerField, SubmitField
 from wtforms.validators import DataRequired, Optional
 
-
 class BboxForm(FlaskForm):
 
     x1 = FloatField("Lon Point 1", validators=[DataRequired()])
